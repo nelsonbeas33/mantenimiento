29744eefc404fd5fff466767535c0e70beb75467
NelsonBeas <nelsonbeas33@gmail.com>
2024-04-22 21:04:08 -0600

    cambio en comentario

--- mantenimiento/Program.cs
+++ mantenimiento/Program.cs
@@ -16,7 +16,6 @@ namespace mantenimiento
             CreateHostBuilder(args).Build().Run();
         }
 
-        //test
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
